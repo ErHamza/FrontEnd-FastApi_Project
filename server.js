f810397bd3ac40e95f298834a472f7fa00d33f92
Hamza Erraji <heerraji123@gmail.com>
2022-11-24 00:58:38 +0100

    secinde

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-const express = require('express');const path = require('path');const app = express();app.use(express.static(__dirname + '/dist/<app-name>'));app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/app/index.html'));});app.listen(process.env.PORT || 8080);
+const express = require('express');const path = require('path');
+const app = express();app.use(express.static(__dirname + '/dist/app'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/app/index.html'))
+;});
+app.listen(process.env.PORT || 8080);
